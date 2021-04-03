d4dc2e9a242d5e79d9453480ca9c73897342c907
rahulreddyp <rahulreddyp77@gmail.com>
2021-04-03 08:07:29 +0530

    initial commit

---
+++ src/Government.java
@@ -0,0 +1,10 @@
+public class Government {
+
+    public Government(String configFile){}
+
+    public boolean mobileContact(String initiator, String contactInfo){return true;}
+
+    public void recordTestResult(String testHash, int date, boolean result){}
+
+    int findGatherings(int date, int minSize, int minTime, float density){return 0;}
+}
---
+++ src/MobileDevice.java
@@ -0,0 +1,12 @@
+public class MobileDevice {
+
+    public MobileDevice(String configFile, Government contactTracer) {
+
+    }
+
+    public void recordContact(String individual, int date, int duration){}
+
+    public void positiveTest(String testHash) {}
+
+    public boolean synchronizeData(){return true;}
+}
